f477d61145337fd697b27a7329027d9531888a71
Patrick Apgar <papgar9@gmail.com>
2018-10-29 04:02:26 +0000

    continued on v6. Added authentication routes and passport configuration as well as logged in middleware. Will finish by tomorrow and continue to make style changes as well as build forms for login/register files.

--- v6/app.js
+++ v6/app.js
@@ -16,7 +16,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
-// Database is now getting ready for setup. Will continue in coming days :)
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Apple Pie",
+    resave: false,
+    saveUnitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 // Initial route setup including root and index route
 app.get("/", function(req, res){
@@ -129,6 +145,26 @@ app.post("/register", function(req, res){
       passport.authenticate()
    });
 });
+
+//show login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+
+//handle login logic
+app.post("/login", passport.authenticate("local", 
+    //create arguments for authorized session
+    {
+        successRedirect: "/index",
+        failureRedirect: "/login"
+    }), function(req, res){});
+    
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.get("*", function(req, res){
     res.send("Uh oh! Page not found!");
