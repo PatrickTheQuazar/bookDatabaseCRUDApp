53d797aa5fa52a110adf1af3f386e96df3c615fb
Patrick Apgar <papgar9@gmail.com>
2018-11-06 05:48:54 +0000

    Observed code and still unable to locate the source of the auth bug. Will continue searching tomorrow.

--- v6/app.js
+++ v6/app.js
@@ -15,19 +15,19 @@ mongoose.connect("mongodb://localhost/book_database_v6");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+// seedDB();
 
 //PASSPORT CONFIGURATION
 app.use(require("express-session")({
-    secret: "Apple Pie",
+    secret: "apple pie",
     resave: false,
     saveUninitialized: false
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
